8dc7cca3eaf7d86e843e5ead80fb96c2958bda97
ASWIN SPARKY <105107888+EX-SPARKY@users.noreply.github.com>
2023-08-22 01:11:10 +0530

    TEAM EX-BOT-Z 🗿🪐

---
+++ settings.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+const chalk = require('chalk')
+  
+global.autoTyping = false
+global.autoRecord = false
+global.autoread = false
+global.alwaysonline = true
+global.statusread = true
+global.private = false
+global.autodlyt = true
+global.ownername = "ᴀsᴡɪɴ sᴘᴀʀᴋʏ"
+global.botname = "x-ʙᴏᴛ-ᴍᴅ"
+global.ownernumber = ["916235636485","917012984396","919746072087","43664773900000"]
+global.caption = "_*ʜᴀᴘᴘʏ ᴀᴀʏɪʟʟᴇ ᴍʏʀᴇ ɴɪ 🗿👍⁉️*_"
+//---------------------------------
+// Owner Mention Audio
+//---------------------------------
+global.ownermention = true
+global.mentionaudio = ["https://i.imgur.com/h0mNy8p.mp4","https://i.imgur.com/zNWjhDn.mp4","https://i.imgur.com/Ya3325t.mp4","https://i.imgur.com/k4FE9mA.mp4","https://i.imgur.com/X5WGE9W.mp4","https://i.imgur.com/veoDgRH.mp4","https://i.imgur.com/CZ3eJgw.mp4","https://i.imgur.com/6QFiAoM.mp4","https://i.imgur.com/lAbt9ew.mp4","https://i.imgur.com/AcHHpqh.mp4","https://i.imgur.com/t7HKmDj.mp4","https://i.imgur.com/oJG0p1R.mp4","https://i.imgur.com/hCnMXIt.mp4","https://i.imgur.com/wduQUZ6.mp4","https://i.imgur.com/Ec1fHg6.mp4","https://i.imgur.com/ul9x6Fl.mp4","https://i.imgur.com/B6DuFxf.mp4","https://i.imgur.com/rTfVyTL.mp4","https://i.imgur.com/f06QQMw.mp4","https://i.imgur.com/hrOA7SZ.mp4","https://i.imgur.com/iXCTeqC.mp4","https://i.imgur.com/prb6egI.mp4","https://i.imgur.com/j8vAgTm.mp4","https://i.imgur.com/u1VIily.mp4","https://i.imgur.com/O36QHKi.mp4","https://i.imgur.com/tZC4JKd.mp4","https://i.imgur.com/KganHHA.mp4","https://i.imgur.com/DVTRwBc.mp4","https://i.imgur.com/LqkVfhp.mp4","https://i.imgur.com/SJiFZUE.mp4","https://i.imgur.com/WARShRy.mp4","https://i.imgur.com/hyQj2lx.mp4","https://i.imgur.com/txkM9qW.mp4","https://i.imgur.com/EprdRNJ.mp4","https://i.imgur.com/pP6NHPf.mp4","https://i.imgur.com/wODIlC7.mp4","https://i.imgur.com/rRPnnCq.mp4","https://i.imgur.com/xnfG7tg.mp4","https://i.imgur.com/k1lq9vs.mp4","https://i.imgur.com/qPkviUd.mp4","https://i.imgur.com/gv29aga.mp4","https://i.imgur.com/8HvdglZ.mp4","https://i.imgur.com/EfYQnb9.mp4","https://i.imgur.com/tIehcgC.mp4","https://i.imgur.com/VwzM3wV.mp4","https://i.imgur.com/WKpl7eS.mp4","https://i.imgur.com/5Wnd52w.mp4","https://i.imgur.com/eM3RypD.mp4","https://i.imgur.com/b5kXK5V.mp4","https://i.imgur.com/uLG516B.mp4","https://i.imgur.com/hP2TXKm.mp4","https://i.imgur.com/cZggJkF.mp4","https://i.imgur.com/RV8CnnC.mp4","https://i.imgur.com/5N26kvL.mp4","https://i.imgur.com/RlOinJI.mp4","https://i.imgur.com/58OLkaJ.mp4","https://i.imgur.com/EiHuGSL.mp4","https://i.imgur.com/tG96OYD.mp4","https://i.imgur.com/qOVkgkM.mp4","https://i.imgur.com/tkEmtyt.mp4","https://i.imgur.com/PgM9o1N.mp4","https://i.imgur.com/WoIAAyb.mp4","https://i.imgur.com/w8ilFA2.mp4","https://i.imgur.com/2AioS69.mp4","https://i.imgur.com/5NuWuit.mp4","https://i.imgur.com/rJl3Fmc.mp4","https://i.imgur.com/MyGVzKY.mp4","https://i.imgur.com/58sgxxs.mp4","https://i.imgur.com/GT48zNT.mp4","https://i.imgur.com/M5qqOOR.mp4","https://i.imgur.com/C6XmNBQ.mp4","https://i.imgur.com/qybdlnH.mp4","https://i.imgur.com/6Mo0t4S.mp4","https://i.imgur.com/7KutCqN.mp4","https://i.imgur.com/vY8CfI7.mp4"] //mention audios
+global.url = 'https://www.instagram.com/aswin_sparky.exe' // mention url
+global.IMGBB_KEY = ["76a050f031972d9f27e329d767dd988f", "deb80cd12ababea1c9b9a8ad6ce3fab2", "78c84c62b32a88e86daf87dd509a657a"]
+global.thumb = "https://i.imgur.com/VIZ1GUr.jpg" // mention thumbnail
