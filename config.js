86767a46bbdaf04c30a50d9b0bdb43f20d29709a
TOXIC-KICHUX <99080094+TOXIC-KICHUX@users.noreply.github.com>
2023-08-23 07:50:01 +0530

    fixed!!

---
+++ config.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const chalk = require('chalk')
+//---------------------------------
+// Only use true/false
+//---------------------------------
+global.autoTyping = false //Auto typing...(true for on, false for off)
+global.autoRecord = false // Auto recording...(true for on, false for off)
+global.autoread = false // Auto Message read(true for on, false for off)
+global.alwaysonline = true // Always Online(true for on, false for off)
+global.statusread = true // Auto status read 🗿🖐🏻(true for on, false for off)
+global.worktype = true // worktype(true for public, false for self)
+global.autodlyt = true // Auto YouTube video downloader(true for on, false for off)
+//---------------------------------
+// Required variables
+//---------------------------------
+global.ownername = "ᴛᴇᴀᴍ ᴇx-ʙᴏᴛ-ᴢ" // Bot Owner name
+global.botname = "x-ʙᴏᴛ-ᴍᴅ" //bot name
+global.ownernumber = ["917012984396","917594898804","919961857267"] //SODO / OWNER NUMBER
+global.caption = "_*ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ x-ʙᴏᴛ-ᴍᴅ*_" // Caption 
+//---------------------------------
+// Owner Mention Audio
+//---------------------------------
+global.ownermention = true // to turn off type false  || turn on type true 👌🏻
+global.thumb = "https://i.imgur.com/VIZ1GUr.jpg" // mention thumbnail
+global.mentionaudio = ["https://i.imgur.com/h0mNy8p.mp4","https://i.imgur.com/zNWjhDn.mp4","https://i.imgur.com/Ya3325t.mp4","https://i.imgur.com/k4FE9mA.mp4","https://i.imgur.com/X5WGE9W.mp4","https://i.imgur.com/veoDgRH.mp4","https://i.imgur.com/CZ3eJgw.mp4","https://i.imgur.com/6QFiAoM.mp4","https://i.imgur.com/lAbt9ew.mp4"] //mention audios
+//---------------------------------
+// All in One Url ( you can add any url you want )
+//---------------------------------
+global.url = 'https://chat.whatsapp.com/JjzEUDkJgke1vPGK3GAvG6' // mention url
+//---------------------------------
+// No need of editing ( Edit aaki oombanda 🗿🖐🏻)
+//---------------------------------
+global.IMGBB_KEY = ["76a050f031972d9f27e329d767dd988f", "deb80cd12ababea1c9b9a8ad6ce3fab2", "78c84c62b32a88e86daf87dd509a657a"]
