4dfb2991fcd4ef33c26fc08453f95da3c10edfd6
Aswin Sparky <113657908+A-S-W-I-N-S-P-A-R-K-Y@users.noreply.github.com>
2023-09-30 00:34:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -44,7 +44,7 @@ global.ownermention = true // to turn off type false  || turn on type true ðŸ‘Œð
 global.mtitle = "X-Bot-Md" // mention tittle
 global.mbody = "TEAM EX BOT-Z" // mention discription 
 global.mthumb = 'https://i.imgur.com/J2x8YMw.jpg' // mention thumbnail 
-global.mmurl = 'https://instagram.com/aswin_sparky.exe'
+global.murl = 'https://instagram.com/aswin_sparky.exe'
 global.mentionaudio = ["https://i.imgur.com/h0mNy8p.mp4","https://i.imgur.com/zNWjhDn.mp4","https://i.imgur.com/Ya3325t.mp4","https://i.imgur.com/k4FE9mA.mp4","https://i.imgur.com/X5WGE9W.mp4","https://i.imgur.com/veoDgRH.mp4","https://i.imgur.com/CZ3eJgw.mp4","https://i.imgur.com/6QFiAoM.mp4","https://i.imgur.com/lAbt9ew.mp4"] //mention audios
 //---------------------------------
 // All in One Url ( you can add any url you want )
