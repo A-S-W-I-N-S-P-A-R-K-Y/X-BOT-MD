1e7eff9fc18721e4c0c86ad893ebc4fb3a99b8a8
Aswin Sparky <113657908+A-S-W-I-N-S-P-A-R-K-Y@users.noreply.github.com>
2023-10-04 23:50:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,59 +1,46 @@
-const fs = require('fs')
-const chalk = require('chalk')
+const { Sequelize } = require("sequelize");
+const fs = require("fs");
+const toBool = (x) => x == "true";
+DATABASE_URL = process.env.DATABASE_URL || "./lib/database.db";
+let HANDLER = "false";
 
-
-
-//-----------------------
-// SESSION ID CONECTING 👇🏻
-//-------------------------
 module.exports = {
-SESSION_ID: '' //Put your session id here
-}
-/*
-
- Other Configurations are down 👇🏻
-
-
-*/
-//---------------------------------
-// Only use true/false
-//---------------------------------
-global.autoTyping = false //Auto typing...(true for on, false for off)
-global.autoRecord = false // Auto recording...(true for on, false for off)
-global.autoread = false // Auto Message read(true for on, false for off)
-global.alwaysonline = true // Always Online(true for on, false for off)
-global.statusread = true // Auto status read 🗿🖐🏻(true for on, false for off)
-global.private = true // worktype(true for private, false for public)
-global.autoreact = false // Auto react ( verum myra aah 🤣🙌🏻 )
-global.autodlyt = false // Auto YouTube video downloader(true for on, false for off)
-global.largethumb = "true" // for large thumbnail 
-global.version = "1.1.1"
-//---------------------------------
-// Required variables
-//---------------------------------
-global.prefix = "." // use "" for removing prefix
-global.ownername = "ᴛᴇᴀᴍ ᴇx-ʙᴏᴛ-ᴢ" // Bot Owner name
-global.botname = "x-ʙᴏᴛ-ᴍᴅ" //bot name
-global.thumb = "https://i.imgur.com/IIHEyLI.jpg" // bot image overall
-global.ownernumber = ["917012984396","917594898804","919961857267"] //SODO / OWNER NUMBER
-global.caption = "_*ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ x-ʙᴏᴛ-ᴍᴅ*_" // Caption 
-//---------------------------------
-// Owner Mention Audio
-//---------------------------------
-global.ownermention = true // to turn off type false  || turn on type true 👌🏻
-global.mtitle = "X-Bot-Md" // mention tittle
-global.mbody = "TEAM EX BOT-Z" // mention discription 
-global.mthumb = 'https://i.imgur.com/J2x8YMw.jpg' // mention thumbnail 
-global.murl = 'https://instagram.com/aswin_sparky.exe'
-global.mentionaudio = ["https://i.imgur.com/h0mNy8p.mp4","https://i.imgur.com/zNWjhDn.mp4","https://i.imgur.com/Ya3325t.mp4","https://i.imgur.com/k4FE9mA.mp4","https://i.imgur.com/X5WGE9W.mp4","https://i.imgur.com/veoDgRH.mp4","https://i.imgur.com/CZ3eJgw.mp4","https://i.imgur.com/6QFiAoM.mp4","https://i.imgur.com/lAbt9ew.mp4"] //mention audios
-//---------------------------------
-// All in One Url ( you can add any url you want )
-//---------------------------------
-global.url = 'https://chat.whatsapp.com/JjzEUDkJgke1vPGK3GAvG6' // mention url
-//---------------------------------
-// No need of editing ( Edit aaki oombanda 🗿🖐🏻) kuduthal edit aakiyal kolamm aayum myra 🖐🏻🤣
-//---------------------------------
-global.IMGBB_KEY = ["76a050f031972d9f27e329d767dd988f", "deb80cd12ababea1c9b9a8ad6ce3fab2", "78c84c62b32a88e86daf87dd509a657a"]
-global.reactemoji = ['😨','😅','😂','😳','😎', '🥵', '😱', '🐦', '🙄', '🐤','🗿','🐦','🤨','🥴','😐','👆','😔', '👀','👎','🔥','💦','✂️','🍭'] // auto react emoji
-global.sparky = ['☆','✼','　 ҉ ','❏','🍭','△'] // menu design's 
-global.worktype = 'X BOT MD MODE' // Dont edit it 💉✨
+  ANTILINK: toBool(process.env.ANTI_LINK) || false,
+  LOGS: toBool(process.env.LOGS) || true,
+  ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
+  SESSION_ID: process.env.SESSION_ID || "",
+  LANG: process.env.LANG || "EN",
+  HANDLERS: process.env.HANDLER || ".",
+  RMBG_KEY: process.env.RMBG_KEY || false,
+  BRANCH: "master",
+  PACKNAME: process.env.PACKNAME || "X-BOT-MD V2",
+  WELCOME_MSG: process.env.WELCOME_MSG || "Hi @user Welcome to @gname",
+  GOODBYE_MSG: process.env.GOODBYE_MSG || "Hi @user It was Nice Seeing you",
+  AUTHOR: process.env.AUTHOR || "Aswin Sparky",
+  DATABASE_URL: DATABASE_URL,
+  SUDO: process.env.SUDO || "917012984396",
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || " ",
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY || " ",
+  OWNER_NAME: process.env.OWNER_NAME || "Aswin Sparky",
+  BOT_NAME: process.env.BOT_NAME || "X-BOT-MD V2",
+  WORK_TYPE: process.env.WORK_TYPE || "public",
+  //---------------------------------------------
+  //Database
+  DATABASE:
+    DATABASE_URL === "./lib/database.db"
+      ? new Sequelize({
+          dialect: "sqlite",
+          storage: DATABASE_URL,
+          logging: false,
+        })
+      : new Sequelize(DATABASE_URL, {
+          dialect: "postgres",
+          ssl: true,
+          protocol: "postgres",
+          dialectOptions: {
+            native: true,
+            ssl: { require: true, rejectUnauthorized: false },
+          },
+          logging: false,
+        }),
+};
