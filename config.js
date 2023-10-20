50a620b86e2d4b9b42a61aa683cedc9695874dfc
Aswin Sparky <113657908+A-S-W-I-N-S-P-A-R-K-Y@users.noreply.github.com>
2023-10-20 16:44:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,8 @@ const chalk = require('chalk')
 // SESSION ID CONECTING 👇🏻
 //-------------------------
 module.exports = {
-SESSION_ID: '' //Put your session id here
+SESSION_ID: '', //Put your session id here
+ALIVE: `HELLO I AM ALIVE`
 }
 /*
 
