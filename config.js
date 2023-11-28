29f57f4ba9930cf7f8dc5d4032d93b5c99f5a685
Aswin Sparky <113657908+A-S-W-I-N-S-P-A-R-K-Y@users.noreply.github.com>
2023-11-28 18:25:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -11,6 +11,7 @@ module.exports = {
   MENTION_DATA: process.env.MENTION_DATA || 'X-BOT-MD;MULTI-DEVICE BOT BY TEAM EX-BOT-Z;https://wa.me/917012984396;https://i.imgur.com/3cMVKBk.jpeg',
   MENTION_AUDIO: process.env.MENTION_AUDIO || ["https://i.imgur.com/h0mNy8p.mp4","https://i.imgur.com/zNWjhDn.mp4","https://i.imgur.com/Ya3325t.mp4","https://i.imgur.com/k4FE9mA.mp4","https://i.imgur.com/X5WGE9W.mp4","https://i.imgur.com/veoDgRH.mp4","https://i.imgur.com/CZ3eJgw.mp4","https://i.imgur.com/6QFiAoM.mp4","https://i.imgur.com/lAbt9ew.mp4"],
   AUTO_STATUS_VIEW: process.env.AUTO_STATUS_VIEW || "true",
+  AUTOBIO: process.env.AUTOBIO || "",
   CAPTION: process.env.CAPTION || "X-BOT-MD V-2.00",
   TENORAPI: process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c",
   PORT: process.env.PORT || "9000",
