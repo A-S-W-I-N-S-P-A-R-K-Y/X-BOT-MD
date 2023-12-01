4adf54ec30619e75c310b1274cbeb4886c5404b7
Aswin Sparky <113657908+A-S-W-I-N-S-P-A-R-K-Y@users.noreply.github.com>
2023-12-01 14:20:46 +0530

    beta

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
-
-global.owner = process.env.SUDO || ["917012984396","917594898804"];
-
+let gg = process.env.SUDO;
+global.owner = gg.split(",");
 module.exports = {
   mongodb: process.env.MONGODB_URL || "",
   BOT_INFO: process.env.BOT_INFO || "X-BOT-MD;Aswin Sparky;https://graph.org/file/9b477aaa6b90bbb46e4be.mp4;https://i.imgur.com/MsNQ8wf.jpg",
