7ca3a9f0609017d4e61ceb25ce0e47a48872e381
Aswin Sparky <113657908+A-S-W-I-N-S-P-A-R-K-Y@users.noreply.github.com>
2023-12-01 14:33:35 +0530

    fixed

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
-let gg = process.env.SUDO;
-global.owner = gg.split(",");
+let oo = process.env.SUDO;
+let mm = process.env.MENTION_AUDIO;
+global.mentionaudio = mm.split(",");
+global.owner = oo.split(",");
 module.exports = {
   mongodb: process.env.MONGODB_URL || "",
   BOT_INFO: process.env.BOT_INFO || "X-BOT-MD;Aswin Sparky;https://graph.org/file/9b477aaa6b90bbb46e4be.mp4;https://i.imgur.com/MsNQ8wf.jpg",
@@ -8,7 +10,7 @@ module.exports = {
   SESSION_ID: process.env.SESSION_ID || "",
   MENTION: process.env.MENTION || "true",
   MENTION_DATA: process.env.MENTION_DATA || 'X-BOT-MD;MULTI-DEVICE BOT BY TEAM EX-BOT-Z;https://wa.me/917012984396;https://i.imgur.com/3cMVKBk.jpeg',
-  MENTION_AUDIO: process.env.MENTION_AUDIO || ["https://i.imgur.com/h0mNy8p.mp4","https://i.imgur.com/zNWjhDn.mp4","https://i.imgur.com/Ya3325t.mp4","https://i.imgur.com/k4FE9mA.mp4","https://i.imgur.com/X5WGE9W.mp4","https://i.imgur.com/veoDgRH.mp4","https://i.imgur.com/CZ3eJgw.mp4","https://i.imgur.com/6QFiAoM.mp4","https://i.imgur.com/lAbt9ew.mp4"],
+  MENTION_AUDIO: global.mentionaudio,
   AUTO_STATUS_VIEW: process.env.AUTO_STATUS_VIEW || "true",
   AUTOBIO: process.env.AUTOBIO || "",
   CAPTION: process.env.CAPTION || "X-BOT-MD V-2.00",
