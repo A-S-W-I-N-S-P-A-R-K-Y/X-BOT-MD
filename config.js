92a24d8253395a2c6b282beb2f1a8ba10aaca7db
gkslqp <163153697+gkslqp@users.noreply.github.com>
2024-03-13 16:57:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ require("dotenv").config();
 let oo = process.env.SUDO;
 let mm = process.env.MENTION_AUDIO;
 global.mentionaudio = mm.split(",");
-global.owner = oo.split(",");
+global.owner = oo.split("KING KAVIYA");
 module.exports = {
   mongodb: process.env.MONGODB_URL || "",
   BOT_INFO: process.env.BOT_INFO || "X-BOT-MD;Aswin Sparky;https://graph.org/file/9b477aaa6b90bbb46e4be.mp4",
   STICKER_DATA: process.env.STICKER_DATA || "X-BOTMD;ASWIN SPARKY",
-  SESSION_ID: process.env.SESSION_ID || "",
+  SESSION_ID: process.env.SESSION_ID || "X-BOT-MD:LookyDrilling",
   MENTION: process.env.MENTION || "true",
   MENTION_DATA: process.env.MENTION_DATA || 'X-BOT-MD;MULTI-DEVICE BOT BY TEAM EX-BOT-Z;https://wa.me/917012984396;https://i.imgur.com/3cMVKBk.jpeg',
   MENTION_AUDIO: global.mentionaudio,
