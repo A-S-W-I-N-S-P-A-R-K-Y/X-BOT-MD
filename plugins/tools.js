36a7c4355510e83c19df93ecd721ef3629b4e31f
Aswin Sparky <113657908+A-S-W-I-N-S-P-A-R-K-Y@users.noreply.github.com>
2024-03-22 22:18:57 +0530

    Update tools.js

--- plugins/tools.js
+++ plugins/tools.js
@@ -110,32 +110,6 @@ client.sendMessage(m.jid, { image :{ url: dll }, caption: "_X BOT MD V3_"}, {quo
 
 Sparky(
     {
-        name: "vdo",
-        fromMe: true,
-        category: "tools",
-        desc: "Finds music from replied Audio",
-    },
-    async ({
-        m, client, args
-    }) => {
-const { generateWAMessageContent } = require('@whiskeysockets/baileys')
-let buff = await m.quoted.download()
-let msg = await generateWAMessageContent({
-            video: buff
-        }, {
-            upload: client.waUploadToServer
-        })
-        await client.relayMessage(args || m.jid, {
-            ptvMessage: msg.videoMessage
-        }, {
-            quoted: m
-        })
-    }
-	);
-
-
-Sparky(
-    {
         name: "save",
         fromMe: true,
         category: "tools",
