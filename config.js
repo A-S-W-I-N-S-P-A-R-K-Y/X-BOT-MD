cdfa34cebdd85750a200d89bbaf65ba3761f338e
kamran522 <107455935+kamran522@users.noreply.github.com>
2024-08-22 23:00:49 +0500

    Update config.js

--- config.js
+++ config.js
@@ -9,19 +9,19 @@ if (fs.existsSync('config.env')) {
 
 const API = "https://api-aswin-sparky.koyeb.app";
 const ALIVE = process.env.ALIVE || "I am Alive";
-const HANDLERS = process.env.HANDLER || ".";
-const SESSION_ID = process.env.SESSION_ID || "A-S-W-I-N-S-P-A-R-K-Y:4f2ac84440db11cee3d71d1cf01dab77";
-const SUDO = process.env.SUDO || '917012984396';
+const HANDLERS = process.env.HANDLER || "";
+const SESSION_ID = process.env.SESSION_ID || "A-S-W-I-N-S-P-A-R-K-Y:b666f1a3a340c80039165aaad4c122f5";
+const SUDO = process.env.SUDO || '923323963788';
 const AUTO_STATUS_VIEW = process.env.AUTO_STATUS_VIEW || "true";
 const ALWAYS_ONLINE = process.env.ALWAYS_ONLINE || "false";
 const DISABLE_PM = process.env.DISABLE_PM  || "false";
 const PM_BLOCK = process.env.PM_BLOCK || "false";
 const PMB = process.env.PMB || "Sorry, I can't help you in private chat.";
 const READ_MESSAGES = process.env.READ_MESSAGES || "false";
-const BOT_INFO = process.env.BOT_INFO || "X-BOT-MD;ASWIN SPARKY;https://i.imgur.com/bZUQlHI.jpeg";
+const BOT_INFO = process.env.BOT_INFO || "X-BOT-MD;K A M I;https://i.imgur.com/bZUQlHI.jpeg";
 const URL = process.env.URL || "https://www.instagram.com/kamran_.hasil";
-const AUDIO_DATA = process.env.AUDIO_DATA || "X BOT MD;ASWIN SPARKY;https://i.imgur.com/bZUQlHI.jpeg";
-const STICKER_DATA = process.env.STICKER_DATA || "X BOT MD;ASWIN SPARKY";
+const AUDIO_DATA = process.env.AUDIO_DATA || "X BOT MD;K A M I;https://i.imgur.com/bZUQlHI.jpeg";
+const STICKER_DATA = process.env.STICKER_DATA || "X BOT MD;K A M I";
 const WORK_TYPE = process.env.WORK_TYPE || 'public';
 const DATABASE_URL = process.env.DATABASE_URL || "./lib/database.db";
 const HEROKU_APP_NAME = process.env.HEROKU_APP_NAME || "";
